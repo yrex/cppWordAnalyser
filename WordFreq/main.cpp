de8e765b6428f7e181dca4df0e39656407530d33
yrex <yrex@users.noreply.github.com>
2015-01-10 17:55:56 +0000

    Initial Commit

---
+++ WordFreq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WordFreq
+//
+//  Created by Soroush Javidi on 10/01/2015.
+//  Copyright (c) 2015 Soroush Javidi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
